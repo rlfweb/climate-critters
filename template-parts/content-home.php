406de29409ff4145d38e96bd9a0a04d4362070a3
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-04-23 16:13:53 +0100

    Roughing out content-home and index

---
+++ template-parts/content-home.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Climate_Critters
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+
+<h1>Welcome!</h1>
+<h2>A catchy tagline here - i.e. Climate Critters is an animated film and a song, which will change the world</h2>
+	</header><!-- .entry-header -->
+
+
+
+	<div class="entry-content">
+
+<h1>The logo and the critters headshots here</h1>
+
+<h1>The dynamic homepage text here</h1>
+
+<h1>Join us now!</h1>
+<h1>Button Block</h1>
+<h1>It's time to make your voices heard</h1>
+
+
+<h1>COP26 2021 UN Climate Change Conference</h1>
+<h1>1–12 Nov 2021 </h1>
+
+
+<div>FIGHT GLOBAL WARMING</div>
+<div>STOP POLLUTION</div> 
+<div>HEAL OUR PLANET</div> 
+<div>SAVE THE WORLD</div> 
+ 
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
