c0b902f075ecc5505323f820838353b4b65af06f
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-04-26 13:05:24 +0100

    Removing Primary from mobile menu button

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'climate-critters' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'climate-critters' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
