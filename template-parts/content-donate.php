b4a3bf1a855eed5c1e02616b132104060b85e20d
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-04-28 14:45:53 +0100

    Video and donate template parts

--- template-parts/content-donate.php
+++ template-parts/content-donate.php
@@ -1,3 +1,15 @@
-<button class="blue-block button-block-button">
+<!-- NOT SURE YET - THIS CODE -->
+
+<div class="button-block mb5">
+<div class="button-block-button blue-block ph3 center">
+    <a href="https://donorbox.org/features">
+    <?php the_sub_field('donate'); ?>
+    </a>
+</div>
+</div>
+
+<!-- OR THIS CODE -->
+
+<!-- <button class="blue-block button-block-button">
 <?php the_sub_field('donate'); ?>
-</button>
+</button> -->
--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -1,3 +1,3 @@
-<div class="video-audio ph3 mv4">
+<div class="video-audio ph3 mb5">
     <?php the_sub_field('video-audio'); ?>
 </div>
