075de89b232e49728ae413ea29301deafdacd057
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-05-03 18:51:26 +0100

    Repeater Fields - change id to class - Varshini

--- template-parts/content-repeater.php
+++ template-parts/content-repeater.php
@@ -1,9 +1,9 @@
-<div id="section" class="content-area">
+<div class="repeater content-area">
 	<div class="holder">
-		<div id="halfimg" class="half<?php the_field('image_width', $image['id']); ?>">
+		<div class="halfimg half<?php the_field('image_width', $image['id']); ?>">
 			<img src="<?php the_sub_field('repeater-image'); ?>">
     </div>
-		<div id="halftext" class="half">
+		<div class="halftext half">
 			<p class="critter-title db"><?php the_sub_field('repeater-title'); ?></p>
 			<p class="db"><?php the_sub_field('repeater-text'); ?></p>
 		</div>
