e83fa8bbad4d5db5c735a12c3ff8d7c6dac7fbe5
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-05-05 13:02:38 +0100

    Varshini's solution image_width in repeater fields

--- template-parts/content-repeater.php
+++ template-parts/content-repeater.php
@@ -1,8 +1,11 @@
 <div class="repeater content-area">
 	<div class="holder">
-		<div class="halfimg half<?php the_field('image_width', $image['id']); ?>">
-			<img src="<?php the_sub_field('repeater-image'); ?>">
-    </div>
+		<?php $image = get_sub_field('repeater-image'); ?>
+		
+		<div class="halfimg half <?php the_field('image_width', $image['id']); ?>">
+			<?php echo wp_get_attachment_image($image['id'], 'full'); ?>
+		</div>
+		
 		<div class="halftext half">
 			<p class="critter-title db"><?php the_sub_field('repeater-title'); ?></p>
 			<p class="db"><?php the_sub_field('repeater-text'); ?></p>
